07d8797420512642325ba24de1c68b79a7d4e366
GavinVasandani <89224315+GavinVasandani@users.noreply.github.com>
2021-08-21 19:51:46 +0400

    Branch to check VS code link to Github

--- websiteInternship.py
+++ websiteInternship.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
 
-    st.write("Hello website! My name is Gavin!");
+    st.write("Hello website! My name is Gavin! Branch edit.");
 
 if __name__ == "__main__":
     main();
